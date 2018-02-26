d34313c9c19b0223450bda0b4f1db980722c4716
yinyanlv <yanlv.yin@gmail.com>
2018-02-26 14:11:32 +0800

    初始化项目

---
+++ src/bugong/Connector.java
@@ -0,0 +1,15 @@
+package bugong;
+
+/**
+ * Created by yanlv.yin on 2018/2/26.
+ */
+public class Connector {
+
+    public void connect() throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
